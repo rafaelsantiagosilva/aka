daf052e183059e310a34666fb9a18eb075a0e344
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2026-01-08 13:02:31 -0300

    feat: adding best fonts

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
 	return (
 		<div className="flex flex-col gap-2 items-center justify-center h-dvh w-dvw bg-linear-to-br from-gray-800 to-gray-950">
 			<header className="text-center">
-				<h1 className="text-4xl italic text-shadow-2xs text-shadow-slate-600 font-bold">
+				<h1 className="text-4xl italic text-shadow-2xs text-shadow-slate-400 font-bold font-heading">
 					404
 				</h1>
 				<h2 className="text-2xl mt-2">Página não encontrada.</h2>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
 	return (
 		<div className="flex flex-col gap-2 items-center justify-center h-dvh w-dvw bg-linear-to-br from-gray-800 to-gray-950">
 			<header className="text-center">
-				<h1 className="text-4xl italic text-shadow-2xs text-shadow-slate-600 font-bold">
+				<h1 className="text-4xl italic text-shadow-2xs text-shadow-slate-400 font-bold font-heading">
 					AKA
 				</h1>
 				<h2 className="text-2xl mt-2">Seu encurtador de URLs!</h2>
